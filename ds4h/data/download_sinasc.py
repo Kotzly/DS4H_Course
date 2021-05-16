d2cff35f1464f38e98ab729a548bdcca374793e2
Kotzly <paulo.augusto.viana@hotmail.com>
2021-05-16 19:00:55 -0300

    Updating downloading functions

--- ds4h/data/download_sinasc.py
+++ ds4h/data/download_sinasc.py
@@ -1,21 +1,17 @@
 
+from pathlib import Path
+from ds4h.data.download import download_ftp
+
 DEFAULT_YEARS = range(2010, 2019 + 1)
 
 def download_dbc(save_path, years=DEFAULT_YEARS):
-    root = Path(save_path)
-
-    #Paulo criou a pasta aqui? 
-    dbc_save_folder = root / "SINASC_DBC"
-    dbf_save_folder = root / "SINASC_DBF"
-    csv_save_folder = root / "SINASC_CSV"
+    dbc_save_folder = Path(save_path)
 
     dbc_save_folder.mkdir(exist_ok=True)
-    dbf_save_folder.mkdir(exist_ok=True)
-    csv_save_folder.mkdir(exist_ok=True)
 
     SINASC_URLS = [
         f"ftp://ftp.datasus.gov.br/dissemin/publicos/SINASC/1996_/Dados/DNRES/DNSP{year}.dbc"
-        for year in range(2010, 2019 + 1)
+        for year in years
     ]  
 
     for url in SINASC_URLS:
