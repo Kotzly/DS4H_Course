380924c4b024ce454491acd4e94345c5690ebacd
Kotzly <paulo.augusto.viana@hotmail.com>
2021-05-16 18:52:53 -0300

    Updating python version

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setuptools.setup(
          "Programming Language :: Python :: 3",
          "Operating System :: OS Independent",
      ],
-     python_requires='>=3.8',
+     python_requires='>=3.6',
      install_requires=required,
  )
