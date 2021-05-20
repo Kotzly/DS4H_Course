0caf30ed2a24a8816d2737d6918ab04ce622232a
Kotzly <paulo.augusto.viana@hotmail.com>
2021-05-19 22:11:37 -0300

    Fixing typo

--- setup.py
+++ setup.py
@@ -7,21 +7,21 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setuptools.setup(
-     name='ds4h',  
-     version='0.1',
-     author="",
-     author_email="",
-     description="",
-     long_description=long_description,
-     long_description_content_type="text/markdown",
-     url="https://github.com/Kotzly/DS4H_Course",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "Operating System :: OS Independent",
-     ],
-     python_requires='>=3.6',
-     install_requires=required,,
+    name='ds4h',  
+    version='0.1',
+    author="",
+    author_email="",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Kotzly/DS4H_Course",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+install_requires=required,
     include_package_data=True,
     package_data={"": ["*.r", "*.R"]},
  )
