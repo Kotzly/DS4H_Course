20888e0a7ecefef6f16ba4e798a6a3a4daaab094
Kotzly <paulo.augusto.viana@hotmail.com>
2021-05-22 18:29:21 -0300

    Adding column to csv parsing

--- ds4h/processing/csv.py
+++ ds4h/processing/csv.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from pathlib import Path
 
-SINASC_COLS = ["DTNASC", "QTDFILVIVO", "QTDFILMORT", "IDADEMAE", "RACACOR", "CODMUNNASC", "ESTCIVMAE", "ESCMAE", "PARTO", "IDANOMAL", "GESTACAO"]
+SINASC_COLS = ["DTNASC", "QTDFILVIVO", "QTDFILMORT", "IDADEMAE", "RACACOR", "CODMUNNASC", "ESTCIVMAE", "ESCMAE", "PARTO", "IDANOMAL", "GESTACAO", "ESCMAE2010"]
 
 def join_sinasc_files(csv_folder, save_path=None, cols=None):
     csv_folder = Path(csv_folder)
