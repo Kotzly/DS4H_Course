78386214c9651d76ed822e11727223dab25f6b67
Kotzly <paulo.augusto.viana@hotmail.com>
2021-05-25 23:04:15 -0300

    Removing README from setup due to errors in pip install

--- src/setup.py
+++ src/setup.py
@@ -1,12 +1,6 @@
 import setuptools
-from pathlib import Path
 
-cwd = Path(__file__).absolute().parent
-
-with open(cwd / "../README.md", "r") as fh:
-    long_description = fh.read()
-
-with open(cwd / 'requirements.txt') as f:
+with open('./requirements.txt') as f:
     required = f.read().splitlines()
 
 setuptools.setup(
@@ -15,8 +9,6 @@ setuptools.setup(
     author="",
     author_email="",
     description="",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/Kotzly/DS4H_Course",
     packages=setuptools.find_packages(),
     classifiers=[
