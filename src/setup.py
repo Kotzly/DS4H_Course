2174756b1a46470be4fe677d08e664243b90f7aa
Kotzly <paulo.augusto.viana@hotmail.com>
2021-05-25 22:55:13 -0300

    Moving packaging files

--- src/setup.py
+++ src/setup.py
@@ -1,22 +1,24 @@
 import setuptools
+from pathlib import Path
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()    
+cwd = Path(__file__).absolute().parent
 
-with open('requirements.txt') as f:
+with open(cwd / "../README.md", "r") as fh:
+    long_description = fh.read()
+
+with open(cwd / 'requirements.txt') as f:
     required = f.read().splitlines()
 
 setuptools.setup(
     name='ds4h',  
-    version='0.1',
+    version='1.0',
     author="",
     author_email="",
     description="",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/Kotzly/DS4H_Course",
-    packages=setuptools.find_packages("src"),
-    package_dir={'': 'src'},
+    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
