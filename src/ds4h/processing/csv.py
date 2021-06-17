ef8d173d6a8b2dc784bb3450a9e4ec9648e85b51
Kotzly <paulo.augusto.viana@hotmail.com>
2021-06-16 23:16:14 -0300

    Case for unexisting columns in old DBC's

--- src/ds4h/processing/csv.py
+++ src/ds4h/processing/csv.py
@@ -4,6 +4,12 @@ import numpy as np
 
 SINASC_COLS = ["DTNASC", "QTDFILVIVO", "QTDFILMORT", "IDADEMAE", "RACACOR", "RACACORMAE", "CODMUNNASC", "ESTCIVMAE", "ESCMAE", "PARTO", "IDANOMAL", "GESTACAO"]
 
+def fix_columns(df):
+    for column in SINASC_COLS:
+        if not column in df.columns:
+            df[column] = np.nan
+    return df
+
 def join_sinasc_files(csv_folder, save_path=None, cols=None):
     csv_folder = Path(csv_folder)
     cols = SINASC_COLS if cols is None else cols
@@ -13,11 +19,9 @@ def join_sinasc_files(csv_folder, save_path=None, cols=None):
 
         print("Loading", csv_filepath.name)
         df = pd.read_csv(csv_filepath, usecols=cols)
+        df = fix_columns(df)
 
-        if union_df is None:
-            union_df = df
-        else:
-            union_df = pd.concat([union_df, df], axis=0)
+        union_df = df if union_df is None else pd.concat([union_df, df], axis=0)
 
     if save_path is not None:
         union_df.to_csv(save_path)
